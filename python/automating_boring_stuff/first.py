d48affc1917e9b6221f37066a428adf116fa2ca4
Rohit Tiwari <rohittiwarirvt@gmail.com>
2017-06-10 07:22:44 +0530

    Added pythond html parser and automating borig stuff python programs

---
+++ python/automating_boring_stuff/first.py
@@ -0,0 +1,8 @@
+print('Hello World')
+print('What is your name?')
+myName = input()
+print("It is good to meet you " + myName)
+print("The length of your name is " + str(len(myName)) )
+print("What is your age?")
+myAge = input()
+print("You Will be " + str(int(myAge)+1) + " in a year.")
